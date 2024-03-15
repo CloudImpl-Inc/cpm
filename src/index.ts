f1a68885ee621d70d1314fc9a415f0b6c7726434
randilfernando <randil.fernando@outlook.com>
2024-03-15 16:42:49 +0530

    make project npm deployable

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+#! /usr/bin/env node
+
 import figlet from 'figlet';
+import {Command} from "commander";
 
 console.log(figlet.textSync('cpm'));
+
+const program = new Command();
+
+program
+    .version("1.0.0")
+    .description("CloudImpl project manager | Your partner in project managing")
+    .parse(process.argv);
+
+const options = program.opts();
+
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+}
