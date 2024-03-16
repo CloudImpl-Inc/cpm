0ee744f4b66821d46dd72d00c6bfadf98a6d4aa8
randilfernando <randil.fernando@outlook.com>
2024-03-16 10:46:18 +0530

    fixed args parsing before plugin load issue

--- src/cli.ts
+++ src/cli.ts
@@ -8,27 +8,31 @@ program
     .version("1.0.0")
     .description("CloudImpl project manager | Your partner in project managing");
 
-// Register plugins
-config.plugins.forEach(async p => {
-    const plugin = ((await import(p)).default as CPMPlugin);
-    console.log(`registering plugin: ${plugin.name()}`)
-    plugin.commands().forEach(c => {
-        program.addCommand(c);
-        console.log(`command registered: ${c.name()}`)
-    });
-})
+const loadPlugins = async () => {
+    // Register plugins
+    for (const p of config.plugins) {
+        const plugin = ((await import(`${process.cwd()}/node_modules/${p}`)).default as CPMPlugin);
+        console.log(`registering plugin: ${plugin.name()}`)
+        plugin.commands().forEach(c => {
+            program.addCommand(c);
+            console.log(`command registered: ${c.name()}`)
+        });
+    }
 
-program
-    .on('command:*', function () {
-        console.error('Unknown command: %s\n\n' +
-            'See --help for a list of available commands\n' +
-            'Or you may have a missing plugin.\n\n' +
-            'Dont worry we will fix this together ;)', program.args.join(' '));
-        process.exit(1);
-    })
-    .parse(process.argv);
-
-if (!process.argv.slice(2).length) {
-    console.log(figlet.textSync('cpm'));
-    program.outputHelp();
+    program
+        .on('command:*', function () {
+            console.error('Unknown command: %s\n\n' +
+                'See --help for a list of available commands\n' +
+                'Or you may have a missing plugin.\n\n' +
+                'Dont worry we will fix this together ;)', program.args.join(' '));
+            process.exit(1);
+        })
+        .parse(process.argv);
 }
+
+loadPlugins().then(() => {
+    if (!process.argv.slice(2).length) {
+        console.log(figlet.textSync('cpm'));
+        program.outputHelp();
+    }
+})
