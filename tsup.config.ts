b1e25d9c08d8d10815b6126460dcb821d9160fb5
randilfernando <randil.fernando@outlook.com>
2024-03-18 16:58:08 +0530

    compile ts files separately

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "tsup";
+import glob from "tiny-glob";
 
-export default defineConfig({
-    entry: ["src/index.ts"],
-    format: ["cjs", "esm"], // Build for commonJS and ESmodules
-    dts: true, // Generate declaration file (.d.ts)
-    splitting: false,
-    sourcemap: true,
-    clean: true,
+export default defineConfig(async () => {
+    return {
+        entry: await glob('./src/**/!(*.d|*.spec).ts'),
+        format: ["cjs", "esm"], // Build for commonJS and ESmodules
+        dts: true, // Generate declaration file (.d.ts)
+        splitting: true,
+        sourcemap: true,
+        clean: true,
+    }
 });
